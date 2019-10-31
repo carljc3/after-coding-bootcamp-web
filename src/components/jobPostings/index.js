11cfae290271edd67fec73bc238c2a04797fcef5
amberlee <amberleeha@gmail.com>
2019-10-31 13:29:14 -0700

    initial comonponents and utils for job api

---
+++ src/components/jobPostings/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+
+class JobPostings extends Component {
+  state = {
+    JobPostings: []
+  };
+
+  componentDidMount() {
+    this.loadJobPostings();
+  }
+
+  loadJobPostings = () => {
+    API.getJobPostings()
+      .then(res =>
+        this.setState({ JobPostings: res.data.listings.listing })
+      )
+      .catch(err => console.log(err));
+  };
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    
+  };
+
+  render() {
+    return (
+     <div>
+         {
+             this.state.JobPostings.map(posting =>{
+                 return (
+                <div className= "container">
+                    <h3>Title: {posting.title}</h3>
+                    <h1>How to Apply: {posting.apply_url} </h1>
+                </div>)
+             })
+         }
+     </div>
+    );
+  }
+}
+
+export default JobPostings;
---
+++ src/components/utils/API.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const url = "http://localhost:3030"
+
+export default {
+  // Gets all jobs
+  getJobPostings: function() {
+    return axios.get(`${url}/api/jobPostings`);
+  }
+};
+
+
