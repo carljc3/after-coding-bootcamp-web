fc07cf4438312bc76a2a48acdd7f45b9be78e951
J-Anne Carlson <Carlsonjc3@gmail.com>
2019-11-11 14:12:36 -0800

    added job card

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,33 +1,33 @@
-import React, { Component } from 'react';
-import API from "../utils/API";
-import axios from "axios";
-import { Redirect } from "react-router-dom";
-import "./Login.css" 
+import React, { Component } from 'react'
+import API from '../utils/API'
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+import './Login.css'
 
 export default class Login extends Component {
   state = {
     isShown: false,
     email: '',
-    username: "",
-    password: "",
+    username: '',
+    password: '',
     loggedInUser: '',
     redirect: false,
-    modalClasses: "modal d-block",
-  };
+    modalClasses: 'modal d-block',
+  }
 
   handleInputChange = event => {
     // Getting the value and name of the input which triggered the change
-    const { name, value } = event.target;
+    const { name, value } = event.target
 
     // Updating the input's state
     this.setState({
       [name]: value
-    });
-  };
+    })
+  }
 
   handleFormSubmit = event => {
     // Preventing the default behavior of the form submit (which is to refresh the page)
-    event.preventDefault();
+    event.preventDefault()
     API.logMeIn({
       username: this.state.username,
       password: this.state.password
@@ -37,11 +37,11 @@ export default class Login extends Component {
         redirect: true
       })
     })
-  };
+  }
 
   renderRedirect = () => {
     if (this.state.redirect) {
-      return <Redirect to='/portfolio' />
+      return <Redirect to='/portfolio'/>
     }
   }
 
@@ -51,46 +51,19 @@ export default class Login extends Component {
     })
   }
 
-  render() {
-
-      return (
-          <div>
-              <div className={this.state.modalClasses} id="modalChefLogInForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-                  aria-hidden="true">
-                  <div className="modal-dialog" role="document">
-                      <div className="modal-content">
-                          <div className="modal-header text-center">
-                              <h4 className="modal-title w-100 font-weight-bold">Login</h4>
-                              <button  onClick={this.props.clickHandler} type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                  <span className= "XButton" aria-hidden="true">&times;</span>
-                              </button>
-                          </div>
-                          <div className="modal-body modal-open mx-3">
-
-                          <h5 className="text-center">Login to your account</h5>
-                              <div className="md-form mb-5">
-                                  {/* <i className="fas fa-user prefix grey-text"></i> */}
-                                  <label className="Username" data-error="wrong" data-success="right" htmlFor="form34">Username:</label>
-                                  <input type="text" required name="username" value={this.state.username} onChange={this.handleInputChange} className="form-control" placeholder="" />
-
-                                  <br />
-                                  {/* <i className="fas fa-lock"></i> */}
-                                  <label className="Password" data-error="wrong" data-success="right" htmlFor="form34">Password:</label>
-                                  <input type="password" required name="password" value={this.state.password} onChange={this.handleInputChange} className="form-control" placeholder="" />
-                              </div>
-                          </div>
-                          <div className="modal-footer d-flex justify-content-center">
-
+  render () {
     return (
       <div>
-        <div className={this.state.modalClasses} id="modalChefLogInForm" tabindex="-1" role="dialog" aria-labelledby="myModalLabel"
-          aria-hidden="true">
+        <div className={this.state.modalClasses} id="modalChefLogInForm" tabindex="-1" role="dialog"
+             aria-labelledby="myModalLabel"
+             aria-hidden="true">
           <div className="modal-dialog" role="document">
             <div className="modal-content">
               <div className="modal-header text-center">
                 <h4 className="modal-title w-100 font-weight-bold">Login</h4>
-                <button onClick={this.props.clickHandler} type="button" className="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
+                <button onClick={this.props.clickHandler} type="button" className="close" data-dismiss="modal"
+                        aria-label="Close">
+                  <span className="XButton" aria-hidden="true">&times;</span>
                 </button>
               </div>
               <div className="modal-body modal-open mx-3">
@@ -98,28 +71,28 @@ export default class Login extends Component {
                 <h5 className="text-center">Login to your account</h5>
                 <div className="md-form mb-5">
                   {/* <i className="fas fa-user prefix grey-text"></i> */}
-                  <label data-error="wrong" data-success="right" htmlFor="form34">Username</label>
-                  <input type="text" required name="username" value={this.state.username} onChange={this.handleInputChange} className="form-control" placeholder="" />
+                  <label className="Username" data-error="wrong" data-success="right" htmlFor="form34">Username:</label>
+                  <input type="text" required name="username" value={this.state.username}
+                         onChange={this.handleInputChange} className="form-control" placeholder=""/>
 
-                  <br />
+                  <br/>
                   {/* <i className="fas fa-lock"></i> */}
-                  <label data-error="wrong" data-success="right" htmlFor="form34">Password</label>
-                  <input type="password" required name="password" value={this.state.password} onChange={this.handleInputChange} className="form-control" placeholder="" />
+                  <label className="Password" data-error="wrong" data-success="right" htmlFor="form34">Password:</label>
+                  <input type="password" required name="password" value={this.state.password}
+                         onChange={this.handleInputChange} className="form-control" placeholder=""/>
                 </div>
               </div>
-              <div className="modal-footer d-flex justify-content-center">
+              <button onClick={this.handleFormSubmit} data-dismiss="modal"
+                      className="login-btn btn btn-info my-4 btn-block" type="submit">Log In
+              </button>
 
-
-
-                <button onClick={this.handleFormSubmit} data-dismiss="modal" className="login-btn btn btn-info my-4 btn-block" type="submit">Log In</button>
-
-              </div>
             </div>
+
+
+            {this.renderRedirect()};
           </div>
         </div>
-
-        {this.renderRedirect()};
-          </div>
+      </div>
     )
   }
 }
